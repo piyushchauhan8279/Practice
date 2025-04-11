c63f150a90dec5e8231f97707538758e698545fb
Piyush Chauhan <piyushchauhan70900@gmail.com>
2025-04-11 16:57:54 +0530

    add files

--- Express/index.js
+++ Express/index.js
@@ -2,11 +2,34 @@
 import 'dotenv/config'
 import express from 'express'
 
+import logger from "./logger.js";
+import morgan from "morgan";
+
+const morganFormat = ":method :url :status :response-time ms";
+
 const app=express()
 const port=process.env.port
 
 app.use(express.json()) // middleware to parse json 
 // hey express when someone pass data in body :- parse it
+
+
+app.use(
+    morgan(morganFormat, {
+      stream: {
+        write: (message) => {
+          const logObject = {
+            method: message.split(" ")[0],
+            url: message.split(" ")[1],
+            status: message.split(" ")[2],
+            responseTime: message.split(" ")[3],
+          };
+          logger.info(JSON.stringify(logObject));
+        },
+      },
+    })
+  );
+
 
 let id = 1;
 let arr = [];
